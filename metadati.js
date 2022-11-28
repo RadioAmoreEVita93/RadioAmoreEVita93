45909c2f96f4c2ee9f7067f5693b1f2ca0c4f330
RadioAmoreEVita93 <109626432+RadioAmoreEVita93@users.noreply.github.com>
2022-11-28 01:00:19 +0100

    Delete metadati.js

--- metadati.js
+++
@@ -1,2 +0,0 @@
-</p><center><iframe scrolling="no" frameborder="0" width="610" height="610" src="https://zenoplay.zenomedia.com/api/zenofm/nowplaying/6bdbnqwnsg8uv?_=1669591157443"></iframe></center>
-
